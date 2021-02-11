fb607a8f96df22494ccf243e2dd98f8e55807652
faisal <faisal25march99@gmail.com>
2021-02-11 11:17:51 +0530

    Find the maximum and minimum element in an array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,58 +1,56 @@
 package com.company;
-
-public class Main {
-
-
-    public static void main(String[] args) {
-/*   Write a program to reverse an array or string
-
-
-    Given an array (or string), the task is to reverse the array/string.
-    Examples :
-
-
-    Input  : arr[] = {1, 2, 3}
-    Output : arr[] = {3, 2, 1}
-
-    Input :  arr[] = {4, 5, 1, 2}
-    Output : arr[] = {2, 1, 5, 4}
-
+/*
+Find the maximum and minimum element in an array
  */
 
-        int [] a = {1,2,3,4,5,6} ;
-        int size = a.length;
-        System.out.println("Before Reversing");
-        printArray(a,size);
-        int start = 0;
-        int end = size - 1 ;
+public class Main {
+    public static void main(String[] args) {
+//        int [] a = {10,1,5,100,20} ;
+        int [] a = {20,18,0,-1,70};
+        System.out.println("Printing array elements");
+        printArray(a);
+       int max = findMaxElement(a);
         System.out.println();
-        System.out.print("After Reversing");
-        reverseArray(a,start,end);
+        System.out.println("Max element in array : " + max);
+
+        int min = findMinElement(a) ;
+
+        System.out.println("Min element in array : " + min);
     }
 
-    private static  void reverseArray(int[] a, int start, int end){
-        int temp ;
-        int size = a.length;
-
-        while(start < end){
-            temp = a[start];
-            a[start] = a[end];
-            a[end] = temp;
-            start++;
-            end--;
+    public static void printArray(int[] a){
+        for(int i : a){
+            System.out.print(i + " ");
         }
-        System.out.println();
-        printArray(a,size);
     }
+    public static int findMaxElement(int[]a){
+        int max = Integer.MIN_VALUE ;
 
-    private static void printArray(int[] a, int size) {
-        for(int i = 0; i < size; i++ ){
-            System.out.print(a[i] + " ");
+        for(int i : a){
+           if (max < i) {
+               max = i ;
+           }
+
         }
+        return max ;
 
     }
 
-
-
-
+    public static int findMinElement(int[] a) {
+        int min = Integer.MAX_VALUE;
+        for(int i : a) {
+            if (min > i) {
+                min =i;
+            }
+        }
+        return min ;
+    }
 }
+
+/*
+Printing array elements
+20 18 0 -1 70
+Max element in array : 70
+Min element in array : -1
+
+ */
