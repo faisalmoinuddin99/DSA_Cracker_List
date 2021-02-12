cd818ce2c2d28b8dcd592e7244d4a1de0ac86183
faisal <faisal25march99@gmail.com>
2021-02-12 09:00:27 +0530

    Sort an array of 0s, 1s and 2s

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,83 +1,68 @@
-package com.company;/*
-MERGE SORT + Given an array arr[] and a number K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+package com.company;
+
+/*
+Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
+
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
 
 Example 1:
 
 Input:
-N = 6
-arr[] = 7 10 4 3 20 15
-K = 3
-Output : 7
-Explanation :
-3rd smallest element in the given
-array is 7.
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated
+into ascending order.
  */
-
 public class Main {
-
-   public static void merge(int A[], int low, int mid, int high){
-    int i,j,k ;
-    int [] B = new int[high + 1];
-     i = low ;
-     j = mid + 1;
-     k = low;
-
-     while(i <= mid && j <= high){
-         if (A[i] < A[j]){
-             B[k++] = A[i++];
-         }else{
-             B[k++] = A[j++] ;
-         }
-     }
-     while(i<= mid) {
-         B[k++] = A[i++];
-     }
-     while(j <= high){
-         B[k++] = A[j++] ;
-     }
-     for(int e = low; e <= high; e ++){
-         A[e] = B[e] ;
-     }
-
-   }
-
-   public static void mergeSort(int[] A,int low, int high){
-       int mid ;
-       if (low < high) {
-           mid = low + (high - low) / 2;
-           mergeSort(A,low,mid);
-           mergeSort(A,mid + 1, high);
-           merge(A,low, mid, high);
-       }
-
-   }
     public static void main(String[] args) {
-        int A[] = {7,10,4,20,15} ;
-        int n = A.length ;
-        int low = 0;
-        int high = n - 1;
-        int mid = low + (high - low) / 2;
-        int k = 4 ;
+        int [] arr = {0,2,1,2,0} ;
+        int size = arr.length;
 
-        mergeSort(A,low,high) ;
-         printArray(A) ;
-        System.out.println();
-        System.out.print("Kth smallest element: ");
-         findKthElement(A,k);
+//        printArray(arr);
+        sort012(arr);
     }
+    public static void sort012(int[] a) {
 
-    public static void printArray(int[] A){
-        for(int i : A){
+        int i, ct0 = 0, ct1 = 0, ct2 = 0;
+
+        for ( i = 0; i < a.length; i++){
+            switch (a[i]){
+                case 0 :
+                    ct0++;
+                    break;
+                case 1:
+                    ct1++;
+                    break;
+                case 2:
+                    ct2++ ;
+                    break;
+            }
+        }
+        i = 0 ;
+        while(ct0 > 0){
+            a[i++] = 0;
+            ct0--;
+        }
+        while(ct1 > 0){
+            a[i++] = 1;
+            ct1--;
+        }
+        while (ct2 > 0){
+            a[i++]  = 2  ;
+            ct2 --;
+        }
+        printArray(a);
+    }
+    public static void printArray(int[] arr){
+        for(int i : arr){
             System.out.print(i + " ");
         }
     }
-    public static void findKthElement(int[] A, int k){
-        System.out.println(A[k - 1]);
-    }
 }
-
 /*
-
-4 7 10 15 20
-Kth smallest element: 15
+output: 0 0 1 2 2
  */
